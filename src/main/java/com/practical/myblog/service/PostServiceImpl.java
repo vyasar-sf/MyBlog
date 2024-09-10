ee6089b84e52b2bfbfac0b45776aa8e4c4196f43
veliyasar <bandedite@gmail.com>
2024-09-10 20:41:31 +0300

    implemented exception handling and loose coupling

--- src/main/java/com/practical/myblog/service/PostServiceImpl.java
+++ src/main/java/com/practical/myblog/service/PostServiceImpl.java
@@ -1,5 +1,7 @@
 package com.practical.myblog.service;
 
+import com.practical.myblog.util.ErrorMessages;
+import com.practical.myblog.exception.PostValidationException;
 import com.practical.myblog.model.Post;
 import com.practical.myblog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +11,37 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
-public class PostService {
-    @Autowired
-    private PostRepository postRepository;
+public class PostServiceImpl implements PostService {
 
+    private final PostRepository postRepository;
+
+    // Constructor injection, making the dependency immutable
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+
+    @Override
     public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
 
+    @Override
     public Post addPost(Post post) {
         if (post.getTitle() == null || post.getTitle().trim().isEmpty()) {
-            throw new IllegalArgumentException("Title cannot be null or empty");
+            throw new PostValidationException(ErrorMessages.TITLE_CANNOT_BE_EMPTY);
         }
         return postRepository.save(post);
     }
 
+    @Override
     public Post getPost(Long id) {
         return postRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Post not found with id: " + id));
+                .orElseThrow(() -> new PostValidationException(ErrorMessages.POST_NOT_FOUND_WITH_ID + id));
     }
 
-
+    @Override
     public Post updatePost(Post post) {
         return postRepository.findById(post.getId())
                 .map(existingPost -> {
@@ -37,7 +49,7 @@ public class PostService {
                     existingPost.setText(post.getText());
                     return postRepository.save(existingPost);
                 })
-                .orElseThrow(() -> new NoSuchElementException("Post not found with id: " + post.getId()));
+                .orElseThrow(() -> new PostValidationException(ErrorMessages.POST_NOT_FOUND_WITH_ID + post.getId()));
     }
 
 }
